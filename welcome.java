98699110fc3db56c8d1931c906736d53352cdbcc
ShuvamSailendraMishra <121077974+ShuvamSailendraMishra@users.noreply.github.com>
2023-03-06 17:07:12 +0530

    Shuvam

--- welcome.java
+++ welcome.java
@@ -6,5 +6,6 @@ public class welcome {
     public static void main(String[] args) {
         System.println("welcome to java technocrat");
         System.out.println("New collaberator added");
+        System.out.println("Shuvam added here");
     }
 }
