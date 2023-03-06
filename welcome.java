f1955e95ff46f70c5912dd8e667b4eb9bc2d126c
Sunil Kumar Dalei <101575041+SunilDalei@users.noreply.github.com>
2023-03-06 17:04:26 +0530

    new collaberator added succesfully

--- welcome.java
+++ welcome.java
@@ -5,5 +5,6 @@ public class welcome {
 
     public static void main(String[] args) {
         System.println("welcome to java technocrat");
+        System.out.println("New collaberator added");
     }
 }
